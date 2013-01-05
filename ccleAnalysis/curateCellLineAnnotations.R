1b1ba199897fd586dff2459d7816358b9878d953
Ubuntu <ubuntu@ip-10-114-15-81.ec2.internal>
2013-01-05 08:50:06 +0000

    refactor createTumorTypeSummary.R

--- ccleAnalysis/curateCellLineAnnotations.R
+++ ccleAnalysis/curateCellLineAnnotations.R
@@ -1,16 +1,8 @@
+require(synapseClient)
+require(rGithubClient)
 
-createTumorTypeSummary <- function(annot, primarySite, fieldToAppend){
-  annotIndices <- which(annot$Primary.site == primarySite)
-  if(primarySite == "haematopoietic_and_lymphoid_tissue"){
-    ####### just use the hist subtype for haematopoietic_and_lymphoid_tissue #############
-    tumorTypeSummaries <- as.character(annot[annotIndices, fieldToAppend])
-  }else{
-    tumorTypeSummaries <- paste(annot[annotIndices, "Primary.site"], annot[annotIndices, fieldToAppend], sep="_")
-  }
-  annot[annotIndices, "Tumor.type.simple"] <- tumorTypeSummaries
-  
-  return(annot)
-}
+repository <- getRepo(repository="/AAMargolin/AdamTestCode")
+sourceRepoFile(repository, "ccleAnalysis/createTumorTypeSummary.R")
 
 curateCellLineAnnotations <- function(){
   sangerAnnotEntity <- loadEntity("syn1577323")
